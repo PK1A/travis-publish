3c06254790650c6858ff3e0d504f085689dd9eb1
pk1a <pkozlowski@amadeus.com>
2014-02-11 16:32:07 +0100

    properly checkout the gh-pages branch

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
-    git checkout gh-pages && git add . && git commit -m "release ${TRAVIS_COMMIT}"
+    git checkout -b origin/gh-pages && git add . && git commit -m "release ${TRAVIS_COMMIT}"
     echo "Build from ${TRAVIS_BRANCH}"
 fi
