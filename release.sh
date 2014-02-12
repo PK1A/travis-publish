25841c11cac7ab2a7fb08db229d354a7d23ba9ea
pk1a <pkozlowski@amadeus.com>
2014-02-12 15:52:08 +0100

    setup user identity in the cloned repo

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
-    # setup git user name / e-mail
-    git config user.email "releasebot@ariatemplates.com"
-    git config user.name "Release Bot"
-    # check-in & push
     git clone https://github.com/PK1A/travis-publish.git temp && cd temp &&
+    git config user.email "releasebot@ariatemplates.com" &&
+    git config user.name "Release Bot" &&
     git checkout -b gh-pages origin/gh-pages &&
     cp -rf ../dist dist &&
     git add -f dist &&
