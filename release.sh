81b36eef62575c3fce0e1e24feb4dac4b4493c7f
pk1a <pkozlowski@amadeus.com>
2014-02-12 14:58:35 +0100

    force-add of the dist folder

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
     # specify gh_user gh_pass as part of the git url
     git remote add github "https://${GH_CREDENTIALS}@github.com/PK1A/travis-publish.git"
     # check-in & push
-    git checkout -b origin/gh-pages && git add dist && git commit -m "release ${TRAVIS_COMMIT}" && git push github gh-pages
+    git checkout -b origin/gh-pages && git add -f dist && git commit -m "release ${TRAVIS_COMMIT}" && git push github gh-pages
     # restore initial branch
     git checkout ${TRAVIS_BRANCH}
 fi
