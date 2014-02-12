224e9e5cb05e1ed59077e6ef91d8629296cfe915
pk1a <pkozlowski@amadeus.com>
2014-02-12 15:44:13 +0100

    use temp folder

--- release.sh
+++ release.sh
@@ -3,10 +3,9 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
     git config user.email "releasebot@ariatemplates.com"
     git config user.name "Release Bot"
     # check-in & push
-    git fetch --all &&
+    git clone https://github.com/PK1A/travis-publish.git temp && cd temp &&
     git checkout -b gh-pages origin/gh-pages &&
     git add -f dist &&
     git commit -m "release ${TRAVIS_COMMIT}" &&
     git push "https://${GH_CREDENTIALS}@github.com/PK1A/travis-publish.git" gh-pages:gh-pages
-    git branch -a
 fi
