daf076703d971204ee44b48485fe854aa7deac4f
pk1a <pkozlowski@amadeus.com>
2014-02-12 15:08:08 +0100

    don't add remotes

--- release.sh
+++ release.sh
@@ -2,10 +2,8 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
     # setup git user name / e-mail
     git config user.email "releasebot@ariatemplates.com"
     git config user.name "Release Bot"
-    # specify gh_user gh_pass as part of the git url
-    git remote add github "https://${GH_CREDENTIALS}@github.com/PK1A/travis-publish.git"
     # check-in & push
-    git checkout -b origin/gh-pages && git add -f dist && git commit -m "release ${TRAVIS_COMMIT}" && git push github gh-pages
+    git checkout -b origin/gh-pages && git add -f dist && git commit -m "release ${TRAVIS_COMMIT}" && git push "https://${GH_CREDENTIALS}@github.com/PK1A/travis-publish.git" gh-pages:gh-pages
     # restore initial branch
     git checkout ${TRAVIS_BRANCH}
 fi
