fab6bc40ceb66cef45311d48bf721e25bbbed5ea
pk1a <pkozlowski@amadeus.com>
2014-02-12 14:49:22 +0100

    push to gh-pages

--- release.sh
+++ release.sh
@@ -1,4 +1,11 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
-    git checkout -b origin/gh-pages && git add . && git commit -m "release ${TRAVIS_COMMIT}"
-    echo "Build from ${TRAVIS_BRANCH}"
+    # setup git user name / e-mail
+    git config user.email "releasebot@ariatemplates.com"
+    git config user.name "Release Bot"
+    # specify gh_user gh_pass as part of the git url
+    git remote add github "https://${GH_CREDENTIALS}@github.com/PK1A/travis-publish.git"
+    # check-in & push
+    git checkout -b origin/gh-pages && git add dist && git commit -m "release ${TRAVIS_COMMIT}" && git push github gh-pages
+    # restore initial branch
+    git checkout ${TRAVIS_BRANCH}
 fi
